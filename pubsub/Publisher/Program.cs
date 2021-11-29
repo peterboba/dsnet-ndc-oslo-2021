2bf6681c301dc09f0638e6d27e61ad434390d12c
Peter Boba <peter.boba@pexip.com>
2021-11-29 15:32:55 +0100

    Added publisher and subscriber

---
+++ pubsub/Publisher/Program.cs
@@ -0,0 +1,19 @@
+﻿using EasyNetQ;
+using Messages;
+string AMQP = "amqps://vkkvkgxw:TJDP2yuJisg98yXtSe2tD2A4LzPFX6RU@hippy-red-gecko.rmq3.cloudamqp.com/vkkvkgxw";
+var bus = RabbitHutch.CreateBus(AMQP);
+
+while(true)
+{
+    Console.WriteLine("Press any key to send a message");
+    Console.ReadKey();
+    var msg = @"
+▄████████▄
+██████████
+█▄██████▄█
+██▄▄▄▄▄▄██
+  ██████
+";
+    var message = new Message(msg);
+    bus.PubSub.Publish(message);
+}
